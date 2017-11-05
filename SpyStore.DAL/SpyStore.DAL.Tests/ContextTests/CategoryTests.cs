8a9d9e9c9ba243d8a72ba3a6a8dbd63840f45d28
gulledgepo <gulledgepo@gmail.com>
2017-11-05 15:55:24 -0800

    INITIAL COMMIT FOR LUIS TO HELP ME FROM LOSING MY MIND

---
+++ SpyStore.DAL/SpyStore.DAL.Tests/ContextTests/CategoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SpyStore2.Models.Entities;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using SpyStore.DAL.EF;
+
+namespace SpyStore.DAL.Tests.ContextTests
+{
+    [Collection("SpyStore.DAL")]
+    public class CategoryTests :IDisposable
+    {
+        private readonly StoreContext _db;
+        public CategoryTests()
+        {
+            _db = new StoreContext();
+            CleanDatabase();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+            CleanDatabase();
+        }
+
+        private void CleanDatabase()
+        {
+            _db.Database.ExecuteSqlCommand("Delete from Store.Categories");
+            _db.Database.ExecuteSqlCommand($"DBCC CHECKIDENT (\"Store.Categories\", RESEED, -1);");
+        }
+
+        [Fact]
+        public void FirstTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ SpyStore.DAL/SpyStore2.Models/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SpyStore2.Models.Entities.Base;
+
+namespace SpyStore2.Models.Entities
+{
+    [Table("Categories", Schema = "Store")]
+    public class Category : EntityBase
+    {
+        [DataType(DataType.Text), MaxLength(50)]
+        public string CategoryName { get; set; }
+    }
+
+
+}
